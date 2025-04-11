fcceefb6ffe1ec32c0dd87d8f2807214fc066f23
Enoch <oyedejienoch@gmail.com>
2025-04-11 04:14:21 +0100

    update speaker details

--- src/sections/Speakers.tsx
+++ src/sections/Speakers.tsx
@@ -7,42 +7,42 @@ const Speakers = () => {
     const slideData = [
         {
           title: "DAYO ADEDEJI",
-          details: "Dayo Adedeji is the founder and Chief Executive officer of Dynasty Africa. A leading marketing agency in Africa that specializes in Influencer Marketing. Dayo Adedeji and his team at Dynasty Africa are on the path to redefine the creative industry and build the largest influencer network in Africa.",
+          details: "Founder/CEO Dynasty Africa",
           src: "/assets/Dayo.jpg",
         },
         {
           title: "OLANREWAJU OLUMIDE",
-          details: "Olanrewaju Olumide is the Managing Partner at Acuity Partners, a Private Client Advisory firm. He is a legal and financial advisor who works with individuals and families to structure their wealth, plan succession and grow their businesses across multiple generations",
+          details: "Managing Director, Acuity",
           src: "/assets/LANRE.jpeg",
         },
         {
           title: "JESUNIMOFE",
-          details: "",
+          details: "Founder ARTXPRESSION",
           src: "/assets/jesunimofe.jpg",
         },
         {
           title: "VICTOR ODULATE",
-          details: "Victor Odulate (popularly called Prof legal expert, project manager, and multidisciplinary thinker with a passion for reimagining systems and optimizing processes. With a background in law, and particularly a penchant for intellectual property, he combines legal expertise with a deep interest in technology, sports, and global affairs.",
+          details: "Legal Expert",
           src: "/assets/Victor.jpg",
         },
         {
           title: "TOBE UGEH",
-          details: "Tobe Ugeh, known as Tobeszn, is a vibrant influencer from Delta State who has captured the hearts of many with his dynamic content. As a Meta ambassador, he shares lifestyle, fashion, and fresh takes on the Nigerian experience, highlighting the country’s beauty and diversity",
+          details: "Influencer/Content Creator",
           src: "/assets/Tobeszn.jpg",
         },
         {
           title: "DEBI JOHNSON",
-          details: "Deborah Johnson is a writer and performance artist, She is an alumni of UNICEF poetry mediathon, a winner of British council's new narratives challenge, a winner of the Doing things with Stories Global play off Contest by the university of Art, Netherlands and Oxfam. ",
+          details: "Writer/Performance Artist",
           src: "/assets/DEBI.JPEG",
         },
         {
           title: "PRECIOUS IBEH",
-          details: "Precious Ibeh is a multifaceted professional dedicated to inspiring and empowering brands and individuals to reach their full potential. As a Brand Strategist, Public Speaker, Event Host, Content Creator, and Travel Connoisseur, he brings a wealth of expertise and a unique perspective to every project he undertakes.",
+          details: "Brand Strategist",
           src: "/assets/PRECIOUS.jpg",
         },
         {
           title: "CHIMEZIE IMO",
-          details: "Chimezie Imo is a Nigerian actor and producer from Imo state, Nigeria. He had featured in films and TV series such as Kasala, MTV Shuga, Kasanova, Eyimofe, Blood Covenant, Otiti, Breath of Life, Everybody Loves Jennifer and many others. He was honored with the trailblazer award for his great impact at the 2024 (AMVCA)",
+          details: "Actor/Producer",
           src: "/assets/Chimezie.jpeg",
         },
       ];
